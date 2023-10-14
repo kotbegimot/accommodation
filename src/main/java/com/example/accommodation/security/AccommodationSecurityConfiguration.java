9013142c2ff88bfc3e5dd74d3b7697a8f058335f
github <arthur.misyunas@gmail.com>
2023-10-14 21:30:43 +0200

    Added swagger bypass support to the security configuration

--- src/main/java/com/example/accommodation/security/AccommodationSecurityConfiguration.java
+++ src/main/java/com/example/accommodation/security/AccommodationSecurityConfiguration.java
@@ -2,11 +2,20 @@ package com.example.accommodation.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+import static com.example.accommodation.util.ControllerUtils.BASE_URL;
 
 @Configuration
+@EnableWebSecurity
 public class AccommodationSecurityConfiguration {
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
@@ -28,6 +37,25 @@ public class AccommodationSecurityConfiguration {
 
         return new InMemoryUserDetailsManager(devon, kirk, nagibator);
     }
+    @Bean
+    public DefaultSecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                        configurer
+                                .requestMatchers(HttpMethod.GET, BASE_URL).hasRole("CUSTOMER")
+                                .requestMatchers(HttpMethod.GET, BASE_URL + "/**").hasRole("CUSTOMER")
+                                .requestMatchers(HttpMethod.POST, BASE_URL).hasRole("MANAGER")
+                                .requestMatchers(HttpMethod.PUT, BASE_URL + "/**").hasRole("MANAGER")
+                                .requestMatchers(HttpMethod.PATCH, BASE_URL + "/book/**").hasRole("MANAGER")
+                                .requestMatchers(HttpMethod.DELETE, BASE_URL + "/**").hasRole("ADMIN")
+                                .requestMatchers("/v2/api-docs/**", "/swagger-ui/**", "/swagger-resources/**",
+                                        "/v2/api-docs/**", "/webjars/**" , "/swagger.json").permitAll()
+                                .anyRequest().authenticated());
 
+        // use HTTP basic authentication
+        http.httpBasic(Customizer.withDefaults());
+        // disable CSRF (Cross Site Request Forgery)
+        http.csrf(AbstractHttpConfigurer::disable);
 
+        return http.build();
+    }
 }
