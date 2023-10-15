36ce20281530a47aa27c36136eaac69358c9a1f0
github <arthur.misyunas@gmail.com>
2023-10-15 14:27:03 +0200

    Finished security configuration for dev profile

--- src/main/java/com/example/accommodation/security/AccommodationSecurityConfiguration.java
+++ src/main/java/com/example/accommodation/security/AccommodationSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.example.accommodation.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,6 +19,7 @@ import static com.example.accommodation.util.ControllerUtils.BASE_URL;
 @EnableWebSecurity
 public class AccommodationSecurityConfiguration {
     @Bean
+    @Profile("dev")
     public InMemoryUserDetailsManager userDetailsManager() {
         UserDetails devon = User.builder()
                 .username("devon")
